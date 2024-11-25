df7e97d85821ff356d60276b4df48e9cf1312175
UnicornDad8 <unicorndad8@gmail.com>
2024-11-25 19:58:19 -0300

    Add elements to home layout, fix next.config.ts, and type all types

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,29 @@
 import { ReactNode } from "react";
+import Sidebar from "@/components/Sidebar";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+// import { Toaster } from "@/components/ui/toaster";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+export const dynamic = "force-dynamic";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <main className="flex h-screen">
-      Sidebar
+      <Sidebar {...currentUser} />
+
       <section className="flex h-full flex-1 flex-col">
-        Mobile Navigation Header
+        <MobileNavigation {...currentUser} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
+
+      {/* Toasts notifications */}
     </main>
   );
 };
