2c8c5e66724953790d64e90e3a2a02aa720de67c
UnicornDad8 <unicorndad8@gmail.com>
2024-11-25 16:15:52 -0300

    Started home layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="flex h-screen">
+      Sidebar
+      <section className="flex h-full flex-1 flex-col">
+        Mobile Navigation Header
+        <div className="main-content">{children}</div>
+      </section>
+    </main>
+  );
+};
+
+export default Layout;
